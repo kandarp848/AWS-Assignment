c5c89e3c945c71e93f36d422c7079b72b15143d4
Kandarp Patel <kpatel@affinionds.com>
2022-09-21 17:42:43 +0530

    FirstCommit

---
+++ AwsAssignment/Controllers/BucketsController.cs
@@ -0,0 +1,45 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwsAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BucketsController : ControllerBase
+    {
+        private readonly IAmazonS3 _s3Client;
+        public BucketsController()
+        {
+            var credentials = new BasicAWSCredentials("AKIAZAYLNII3XQKBSNPH", "rzUyNbJnynI85xRVfjWZCla2BZnNfZ6YCELbL0R3");
+            _s3Client = new AmazonS3Client(credentials, Amazon.RegionEndpoint.APSouth1);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateBucketAsync(string bucketName)
+        {
+            var bucketExists = await _s3Client.DoesS3BucketExistAsync(bucketName);
+            if (bucketExists) return BadRequest($"Bucket {bucketName} already exists.");
+            await _s3Client.PutBucketAsync(bucketName);
+            return Ok($"Bucket {bucketName} created.");
+        }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllBucketAsync()
+        {
+            var data = await _s3Client.ListBucketsAsync();
+            var buckets = data.Buckets.Select(b => { return b.BucketName; });
+            return Ok(buckets);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteBucketAsync(string bucketName)
+        {
+            await _s3Client.DeleteBucketAsync(bucketName);
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ AwsAssignment/S3ObjectDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AwsAssignment
+{
+    public class S3ObjectDto
+    {
+        public string? Name { get; set; }
+        public string? PresignedUrl { get; set; }
+    }
+}
